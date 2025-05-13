cad03fc67300e11ab2b760b42952d5f5849d8944
Sujitha Chapineni <sujithachapineni@Sujithas-MacBook-Air.local>
2025-05-13 16:08:30 +0530

    C3 recruitment problems

--- rivarly.c
+++ rivarly.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef long long ll;
-
 int iscube(long long int x) {
-    long long int left = 1, right = 1000000;
-    while (left <= right) {
-        long long int mid = (left + right) / 2;
-        long long int cube = mid * mid * mid;
-        if (cube == x) 
-        {
-            return 1;
-        }
-        if (cube < x) 
-        {
-            left = mid + 1;
-        }
-        else 
-        {
-            right = mid - 1;
-        }
-    }
-    return 0;
+    long long int fin= round(cbrt((double)x));
+    return fin * fin * fin == x;
 }
 
 int main() {
     int t;
     scanf("%d", &t);
     while (t--) {
-        long long a, b;
+        long long int a, b;
         scanf("%lld %lld", &a, &b);
-        long long product = a * b;
-        if (iscube(product)) 
-        {
+        long long int product = a * b;
+        if (iscube(product)) {
             printf("Yes\n");
-        }
-        else 
-        {
+        } else {
             printf("No\n");
         }
     }
